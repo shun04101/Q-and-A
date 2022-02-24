940e2e1a307feab809de909775c8d777df6a1132
shun04101 <80952330+shun04101@users.noreply.github.com>
2022-02-24 14:58:57 +0000

    [Add]編集/削除

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -23,6 +23,23 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to root_path, notice: 'Update Succes!'
+    else
+      flash[:alert] = "Update Save error!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to root_path, notice: 'Delete Succes!'
   end
 
   private
